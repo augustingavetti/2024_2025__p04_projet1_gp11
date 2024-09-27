dd720b6ac0cb681e7015d288bd0406b95a99b11a
Tristanbonniere <tristan.bonniere@orange.fr>
2024-09-27 15:41:52 +0200

    Modif data + main

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 # bases:
 binary =  0, 1
 decimal = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-hexadecimal = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+hexadecimal = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
 
 # question
 ask_for_the_init_number = int(input("Choisir un nombre de départ:"))
--- main.py
+++ main.py
@@ -13,6 +13,15 @@ assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
 def init_target ():
     init_number = ask_for_the_init_number ()
     init_base = ask_for_the_init_base ()
+    for elmt in init_base:
+        if init_base == [0, 1] and init_number == [0, 1]:
+            target_base
+        elif init_base == decimal and init_number == decimal:
+            target_base
+        elif init_base == binary and init_number == binary:
+            target_base
+        else:
+            print ("Invalid input! Please enter a valid number and base.")
     target_base = ask_for_the_target_base ()
     target_number = \
       bin_dec_hex__to__bin_dec_hex (init_number, \
