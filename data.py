1d099a61726dde331da4e20f31bcc1f30aa6ca53
Tristanbonniere <tristan.bonniere@orange.fr>
2024-09-27 14:58:24 +0200

    Update data
    Bases(number) + question

---
+++ data.py
@@ -0,0 +1,9 @@
+# bases:
+binary =  0, 1
+decimal = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+hexadecimal = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+
+# question
+ask_for_the_init_number = int(input("Choisir un nombre de départ:"))
+ask_for_the_init_base = int(input("Choisissez la base initiale:"))
+ask_for_the_target_base = int(input("Choisissez la base d'arriver:"))
