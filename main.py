60f44ed5e3da53ce06eb32b6fbe527a6918d1ff8
Tristanbonniere <tristan.bonniere@orange.fr>
2024-09-30 11:22:42 +0200

    Add function math

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
 def init_target ():
     init_number = ask_for_the_init_number ()
     init_base = ask_for_the_init_base ()
-    for elmt in init_base:
+    for nb in init_base:
         if init_base == bin_valid and init_number == bin_valid:
             target_base
         elif init_base == dec_valid and init_number == dec_valid:
@@ -23,6 +23,13 @@ def init_target ():
         else:
             print ("Invalid input! Please enter a valid number and base.")
     target_base = ask_for_the_target_base ()
+    if target_base == bin_valid and target_base == dec_valid and target_base ==  hex_valid:
+        hex_math
+    elif target_base == bin_valid and target_base ==  dec_valid:
+        dec_math
+    else:
+        bin_math
+
     target_number = \
       bin_dec_hex__to__bin_dec_hex (init_number, \
                                     init_base, \
---
+++ tools.py
@@ -0,0 +1,6 @@
+def hex_math():
+    pass
+def dec_math():
+    pass
+def bin_math():
+    pass
