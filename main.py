fb986c006832a123eb5ae90b9af1135ce5ed01a6
Tristanbonniere <tristan.bonniere@orange.fr>
2024-09-30 12:09:41 +0200

    def init number

--- main.py
+++ main.py
@@ -10,8 +10,25 @@ def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
 
 assert bin_dec_hex__to__bin_dec_hex ("101", 2, 10) == "5"
 
+def check_char_is_valid(char):
+    return char in hex_valid_chars
+
+def is_a_valid_number(number):
+    i = 0
+    while is_valid_char:
+        is_valid_char = check_char_is_valid (number [i])
+        i = i + 1
+    return is_valid_char
+
+def ask_for_init_number():
+    init_number = input(ask_for_init_number_text)
+    while not (is_a_valid_number (init_number)):
+        init_number = input(ask_again_for_init_number_text)
+    return  init_number
+
+
 def init_target ():
-    init_number = ask_for_the_init_number ()
+    init_number = ask_for_init_number ()
     init_base = ask_for_the_init_base ()
     for nb in init_base:
         if init_base == bin_valid_chars and init_number == bin_valid_chars:
