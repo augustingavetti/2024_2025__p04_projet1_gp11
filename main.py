aa11c0eb064ce7c0e1e03c57621d4a59f07d611e
Tristanbonniere <tristan.bonniere@orange.fr>
2024-09-30 11:53:47 +0200

    logical change

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ def init_target ():
     init_number = ask_for_the_init_number ()
     init_base = ask_for_the_init_base ()
     for nb in init_base:
-        if init_base == bin_valid and init_number == bin_valid:
+        if init_base == bin_valid_chars and init_number == bin_valid_chars:
             target_base
-        elif init_base == dec_valid and init_number == dec_valid:
+        elif init_base == dec_valid_chars and init_number == dec_valid_chars:
             target_base
-        elif init_base == hex_valid and init_number == hex_valid:
+        elif init_base == hex_valid_chars and init_number == hex_valid_chars:
             target_base
         else:
             print ("Entrée invalide! Entrez une nombre et une base valide s'il vous plait")
     target_base = ask_for_the_target_base ()
-    if target_base == bin_valid and target_base == dec_valid and target_base ==  hex_valid:
+    
+    
+    if target_base == bin_valid_chars and target_base == dec_valid_chars and target_base ==  hex_valid_chars:
         hex_math
-    elif target_base == bin_valid and target_base ==  dec_valid:
+    elif target_base == bin_valid_chars and target_base ==  dec_valid_chars:
         dec_math
     else:
         bin_math
