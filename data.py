41185a09046a2bc09eedded6cae8acb30a63aa84
Tristanbonniere <tristan.bonniere@orange.fr>
2024-10-01 09:32:44 +0200

    Data function

--- data.py
+++ data.py
@@ -3,8 +3,11 @@ bin_valid_chars =  ["0", "1"]
 dec_valid_chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 hex_valid_chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
 
+
 # question
-ask_for_init_number_text =  "Please enter the initial number: "
-ask_again_for_init_number_text = "Please enter again the number:"
-ask_for_init_base_text =  "Please enter the initial base: "
-ask_for_init_base_again_text = "Please enter again the base: "
+ask_for_init_base_text = "Enter the initial base (2, 10, or 16): "
+ask_again_for_init_base_text = "Invalid base. Please enter 2, 10, or 16: "
+ask_for_init_number_text = "Enter the number to convert: "
+ask_again_for_init_number_text = "Invalid number for the given base. Please try again: "
+ask_for_target_base_text = "Enter the target base (2, 10, or 16): "
+ask_again_for_target_base_text = "Invalid base. Please enter 2, 10, or 16: "
