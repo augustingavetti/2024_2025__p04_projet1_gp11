57f944b4185534f2cf7113087f371ba66d8bddc0
Tristanbonniere <tristan.bonniere@orange.fr>
2024-10-01 09:12:58 +0200

    Init base

--- data.py
+++ data.py
@@ -6,3 +6,5 @@ hex_valid_chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",
 # question
 ask_for_init_number_text =  "Please enter the initial number: "
 ask_again_for_init_number_text = "Please enter again the number:"
+ask_for_init_base_text =  "Please enter the initial base: "
+ask_for_init_base_again_text = "Please enter again the base: "
