98a7870d63abf0e37ce5f4228ede5c87b42dadb0
Augustin Gavetti <augustin@gavetti.com>
2024-10-01 09:17:06 +0200

    update tools.py hex and dec to bin

--- tools.py
+++ tools.py
@@ -1,11 +1,14 @@
 def hex_math():
     pass
-def dec_math():
-    pass
 def bin_math():
-    if init_base= bin:
-        def hex_dec_to_bin (init_number):
-        init_number ==n
-        while n>0:
-            n=n%2
-            (n//2)%2
+    if init_base == "bin":
+        def hex_dec_to_bin(init_number):
+            n = init_number
+            result = ""
+            while n > 0:
+                result = str(n % 2) + result
+                n = n // 2
+            
+            result += "+1" if init_number % 2 == 1 else "+0"
+            
+            return result
