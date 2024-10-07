38ecb63d7d899e518542eeeba781c33156ae2619
Tristanbonniere <tristan.bonniere@orange.fr>
2024-10-07 12:03:13 +0200

    FIX TOOLS

--- tools.py
+++ tools.py
@@ -18,32 +18,25 @@ def dec_to_bin(init_number):
     return result
             
        
-    def hex_to_bin(init_number):
-                hex_to_binary= {
-                    '0': '0000', '1': '0001', '2': '0010', '3': '0011',
-                    '4': '0100', '5': '0101', '6': '0110', '7': '0111',
-                    '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
-                    'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'
-                }
-                
-                result = ""
-                for digit in init_number.upper():
-                    if digit in hex_to_binary:
-                        result += hex_to_binary[digit]
-                    else:
-                        raise ValueError(f"Invalid hexadecimal digit: {digit}")
-                
-                result = result.lstrip('0')
-                
-            
-                if not result:
-                    return '0'
-                
-                
-                last_digit = init_number[-1].upper()
-                result += "+1" if int(hex_to_binary[last_digit], 2) % 2 == 1 else "+0"
-                
-                return result
+def hex_to_bin(init_number):
+    hex_to_binary= {
+    '0': '0000', '1': '0001', '2': '0010', '3': '0011',
+    '4': '0100', '5': '0101', '6': '0110', '7': '0111',
+    '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
+    'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'
+                } 
+    result = ""
+    for digit in init_number.upper():
+        if digit in hex_to_binary:
+            result += hex_to_binary[digit]
+        else:
+            raise ValueError(f"Invalid hexadecimal digit: {digit}")
+        result = result.lstrip('0')
+        if not result:
+            return '0'
+        last_digit = init_number[-1].upper()
+        result += "+1" if int(hex_to_binary[last_digit], 2) % 2 == 1 else "+0"
+        return result
     
 def bin_math(init_base, init_number):
     if target_base == "bin" and init_base == "dec":
