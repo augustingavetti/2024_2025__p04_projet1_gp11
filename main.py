af8e9ca941efd8a0d5dc344d0631ce611ed561e2
Tristanbonniere <tristan.bonniere@orange.fr>
2024-10-15 09:34:06 +0200

    Added main

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ def ask_for_init_base():
         init_base = input(ask_again_for_init_base_text)
     return  init_base
 
+def ask_for_the_target_base():
+    init_target = input(ask_for_target_base_text)
+    while not (is_a_valid_base (init_target)):
+        init_target = input(ask_again_for_target_base_text)
+    return  init_target
+
 def check_char_is_valid(char):
     return char in hex_valid_chars
 
@@ -47,12 +53,36 @@ def init_target ():
     init_number = ask_for_init_number ()
     init_base = ask_for_init_base ()
     target_base = ask_for_the_target_base ()
+    return init_number, init_base, target_base
+
+def dec_math():
+    init_base = init_target()[1]
+    init_number = init_target()[0]
+    if init_base == "bin":
+        return bin_to_dec(init_number)
+    elif init_base == "hex":
+        return hex_to_dec(init_number)
+    else:
+        raise ValueError("Invalid base")
 
 
-    target_number = \
-      bin_dec_hex__to__bin_dec_hex (init_number, \
-                                    init_base, \
-                                    target_base)
+
+def bin_math():
+    target_base = init_target()[2]
+    init_base = init_target()[1]
+    init_number = init_target()[0]
+    if target_base == "bin" and init_base == "dec":
+        return dec_to_bin(int(init_number, 2))
+    elif target_base == "bin" and init_base == "hex" :
+        return hex_to_bin(init_number)
+    else:
+        raise ValueError("Invalid base")
+
+
+# target_number = \
+#       bin_dec_hex__to__bin_dec_hex (init_number, \
+#                                     init_base, \
+#                                     target_base)
 
 
 
@@ -77,20 +107,3 @@ init_target ()
 
 
 
-# def dec_math(init_base, init_number):
-#     if init_base == "bin":
-#         return bin_to_dec(init_number)
-#     elif init_base == "hex":
-#         return hex_to_dec(init_number)
-#     else:
-#         raise ValueError("Invalid base")
-
-
-
-# def bin_math(init_base, init_number):
-#     if target_base == "bin" and init_base == "dec":
-#         return dec_to_bin(int(init_number, 2))
-#     elif target_base == "bin" and init_base == "hex" :
-#         return hex_to_bin(init_number)
-#     else:
-#         raise ValueError("Invalid base")
