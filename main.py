e70bed0f6e701d676cc47ea65536fd5820ce3efb
Tristanbonniere <tristan.bonniere@orange.fr>
2024-10-15 09:47:52 +0200

    minor

--- main.py
+++ main.py
@@ -50,9 +50,9 @@ def init_target ():
 def dec_math():
     init_base = init_target()[1]
     init_number = init_target()[0]
-    if init_base == "bin":
+    if init_base == 2:
         return bin_to_dec(init_number)
-    elif init_base == "hex":
+    elif init_base == 16:
         return hex_to_dec(init_number)
     else:
         raise ValueError("Invalid base")
@@ -63,9 +63,9 @@ def bin_math():
     target_base = init_target()[2]
     init_base = init_target()[1]
     init_number = init_target()[0]
-    if target_base == "bin" and init_base == "dec":
+    if target_base == 2 and init_base == 10:
         return dec_to_bin(int(init_number, 2))
-    elif target_base == "bin" and init_base == "hex" :
+    elif target_base == 2 and init_base == 16 :
         return hex_to_bin(init_number)
     else:
         raise ValueError("Invalid base")
